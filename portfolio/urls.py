6a32e3a3de978000cd73d6515415c15006d45a94
Sourabh Burse <sourabh.burse@nisevatech.in>
2021-01-18 21:06:26 +0530

    Created Simple Page with simple style

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,9 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from portfolio import *
-
+from .views import *
 urlpatterns = [
-                  path('', include('portfolio.urls')),
-
+                 path('', index, name="index")
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
